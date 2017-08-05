066f8592906a8e95d8e61afcc23d936735abbe37
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2017-08-05 00:18:32 -0300

    Commenting code

--- src/LCD.c
+++ src/LCD.c
@@ -100,27 +100,27 @@ void LCD_setup(LCD *lcd)
 void LCD_init(LCD *lcd)
 {
             LCD_setup(lcd);
-            //Coloca pinos de controle em 0
+            //Set control pins to low level
             GPIOPinWrite(LCD_CTRL_PORT, LCD_CTRL_BUS(lcd), 0x00);
-            // Garante que os pinos RS, RW e ENABLE  estão em 0 (low)
-            //Coloca pinos de dados em 0
+            //Ensures that pins RS, RW and ENABLE will be in 0 (low)
+            //Set data pins to low level
             GPIOPinWrite(LCD_DATA_PORT, LCD_DATA_BUS(lcd), 0x00);
-            //Garante que o barramento de dados esta em 0 (low)
-            SysCtlDelay(DELAY_15ms);            //Tempo para estabilizar LCD
+            //Ensures that data bus will be in 0 (low)
+            SysCtlDelay(DELAY_15ms);            //Time to stabilize the LCD
 
-            // Sequencia de comandos para inicializar e configurar LCD
-            LCD_sendNibble(lcd, 0x03);          // Envia comando para inicializar o display
-            SysCtlDelay(DELAY_5ms);             // Aguarda 5ms para estabilizar o LCD
-            LCD_sendNibble(lcd, 0x03);          // Envia comando para inicializar o display
-            SysCtlDelay(DELAY_5ms);             // Aguarda 5ms para estabilizar o LCD
-            LCD_sendNibble(lcd, 0x03);          // Envia comando para inicializar o display
-            SysCtlDelay(DELAY_5ms);             // Aguarda 5ms para estabilizar o LCD
-            LCD_sendNibble(lcd, 0x02);          // CURSOR HOME - Envia comando para zerar o contador de caracteres e retornar à posição inicial (0x80).
-            SysCtlDelay(DELAY_5ms);             // Aguarda 5ms para estabilizar o LCD
-            LCD_sendByte(lcd, 0, FOUR_BIT);     // FUNCTION SET - Configura o LCD para 4 bits e 2 linhas, fonte 5X7.
-            LCD_clear(lcd);                     // Limpa o LCD
-            LCD_sendByte(lcd, 0, LCD_TURN_ON);  // DISPLAY CONTROL - Display ligado, sem cursor
-            LCD_sendByte(lcd, 0, SHIFT_OFF);    // ENTRY MODE SET - Desloca o cursor para a direita
+            // Commands sequence to initialize and configure the LCD
+            LCD_sendNibble(lcd, 0x03);          // Send first command to initialize the display
+            SysCtlDelay(DELAY_5ms);             // Wait 5ms to stabilize the LCD
+            LCD_sendNibble(lcd, 0x03);          // Send second command to initialize the display
+            SysCtlDelay(DELAY_5ms);             // Wait 5ms to stabilize the LCD
+            LCD_sendNibble(lcd, 0x03);          // Send third command to initialize the display
+            SysCtlDelay(DELAY_5ms);             // Wait 5ms to stabilize the LCD
+            LCD_sendNibble(lcd, 0x02);          // CURSOR HOME - Send commando to go to initial position (0x80)
+            SysCtlDelay(DELAY_5ms);             // Wait 5ms to stabilize the LCD
+            LCD_sendByte(lcd, 0, FOUR_BIT);     // FUNCTION SET - Configures the LCD for 4 bits and 2 lines, and font size 5X7.
+            LCD_clear(lcd);                     // Clear the display
+            LCD_sendByte(lcd, 0, LCD_TURN_ON);  // DISPLAY CONTROL - Turn on display without cursor
+            LCD_sendByte(lcd, 0, SHIFT_OFF);    // ENTRY MODE SET - The cursor will move to right when a new character is being write on display
 }
 
 /**
@@ -129,7 +129,7 @@ void LCD_init(LCD *lcd)
  * @param lcd Pointer to a lcd structure.
  * @see LCD_sendNibble()
  */
-void LCD_clear(LCD *lcd) //Limpa display
+void LCD_clear(LCD *lcd) //Clear the display
 {
     LCD_sendByte(lcd, 0, LCD_CLEAR);
     SysCtlDelay(DELAY_5ms);
@@ -172,7 +172,7 @@ void LCD_sendNibble(LCD *lcd, unsigned char data)
  */
 void LCD_sendByte(LCD *lcd, bool rs, unsigned char byte)
 {
-    GPIOPinWrite(LCD_CTRL_PORT, lcd->en, 0); //garantia que desativa enable
+    GPIOPinWrite(LCD_CTRL_PORT, lcd->en, 0); //Ensure that enable is in low level
 
     if(rs)
     {
@@ -184,8 +184,8 @@ void LCD_sendByte(LCD *lcd, bool rs, unsigned char byte)
         GPIOPinWrite(LCD_CTRL_PORT, lcd->rs, 0); //RS = 0 Intruction
     }
 
-    LCD_sendNibble(lcd, byte >> 4); // Envia a parte ALTA do dado/comando
-    LCD_sendNibble(lcd, (byte & 0x0f));  // Limpa a parte ALTA e envia a parte BAIXA do dado/comando
+    LCD_sendNibble(lcd, byte >> 4); // Send HIGH part of data/command
+    LCD_sendNibble(lcd, (byte & 0x0f));  // Clean the HIGH part and send the LOW part of data/command
 }
 
 /**
@@ -215,16 +215,16 @@ void LCD_inc_cnt(LCD *lcd)
 {
     if(++(lcd->column) > 16)
     {
-        lcd->column = 1; //contando o novo character
+        lcd->column = 1; //Counting the new character
 
         if(lcd->line == 1)
         {
-            LCD_sendByte(lcd, 0, 0xC0); //pula pra segunda linha
+            LCD_sendByte(lcd, 0, 0xC0); //jump for second line
             lcd->line = 2;
         }
         else if(lcd->line == 2)
         {
-            LCD_sendByte(lcd, 0, 0x80); //pula pra primeira linha
+            LCD_sendByte(lcd, 0, 0x80); //jump for first line
             lcd->line = 1;
         }
     }
@@ -239,22 +239,23 @@ void LCD_inc_cnt(LCD *lcd)
  * @see LCD_create()
  * @see LCD_sendByte()
  */
-void LCD_setPosition(LCD *lcd, unsigned int line, unsigned int col) //define a posicão de inicio
+void LCD_setPosition(LCD *lcd, unsigned int line, unsigned int col) //Defines the position of cursor
 {
+    //The first position begins with value 1
     if(line == 1)
     {
-        line = 0x80;     //Se setado linha 1, end incial 0x80
+        line = 0x80;     //If 1, initial address is 0x80
         lcd->line = 1;
     }
     else if(line == 2)
     {
-        line = 0xC0;     //Se setado linha 2, end incial 0xc0
+        line = 0xC0;     //If 2, initial address is 0xC0
         lcd->line = 2;
     }
 
-    lcd->column = col - 1;         //subtrai 1 para corrigir posição
+    lcd->column = col - 1;         //Subtract 1 to correct position
 
-    line = line + lcd->column;     //soma ao end inicial, o numero da coluna
+    line = line + lcd->column;     //add to initial address the number of column
 
 
     LCD_sendByte(lcd, 0, (char)line);
